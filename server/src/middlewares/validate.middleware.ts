e1f9936793f65362fa63becbf4ae3d80bbd31607
Deep Prajapati <dvprajapati7212@gmail.com>
2026-05-19 17:24:30 +0530

    feat: add Zod validation middleware for request data validation

---
+++ server/src/middlewares/validate.middleware.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+import z from "zod";
+
+type ValidateTarget = "body" | "params" | "query"
+
+const validate = (schema: z.ZodType, target: ValidateTarget = "body") => (req: Request, _res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req[target]);
+
+    if (!result.success) {
+        const errors = result.error.issues.map((issue) => ({
+            field: issue.path.join("."),
+            message: issue.message,
+        }))
+
+        return next({
+            statusCode: 400,
+            message: errors[0]?.message,
+            errors
+        })
+    }
+    req[target] = result.data;
+    next();
+}
+
+export default validate
