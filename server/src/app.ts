53e60cda6cf9af3beb969c42c4b64a38fa22ae68
Deep Prajapati <dvprajapati7212@gmail.com>
2026-05-31 16:11:53 +0530

    feat: implemented  testimonial response routes and add in app file

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,6 +5,7 @@ import { env } from "./config/env";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes"
 import requestRoutes from "./routes/request.routes"
+import responseRoutes from "./routes/response.routes"
 const app = express();
 
 app.use(cors({
@@ -17,7 +18,8 @@ app.get("/", (_req, res) => {
     res.send("Speakwell API is running");
 })
 app.use("/api/auth", authRoutes)
-app.use("/api/requests",requestRoutes)
+app.use("/api/requests", requestRoutes)
+app.use("/api/responses",responseRoutes)
 app.use("/{*splat}", (_req, res) => {
     res.status(404).json({
         success: false,
---
+++ server/src/routes/response.routes.ts
@@ -0,0 +1,55 @@
+import { Router } from 'express';
+import { protect } from '../middlewares/auth.middleware';
+import validate from '../middlewares/validate.middleware';
+import { submitResponseSchema, approveResponseSchema, responseIdParamSchema, tokenParamSchema, requestIdParamSchema } from '../validations/response.validation';
+import { submitResponseController, getResponseByRequestController, getResponseByIdController, approveResponseController, rejectResponseController, togglePublishResponseController, deleteResponseController } from '../controllers/response.controller';
+
+const router = Router();
+
+router.post(
+    "/submit/:token",
+    validate(tokenParamSchema, "params"),
+    validate(submitResponseSchema),
+    submitResponseController
+)
+
+router.use(protect)
+
+router.get(
+    "/request/:requestId",
+    validate(requestIdParamSchema, "params"),
+    getResponseByRequestController
+)
+
+router.get(
+    "/:id",
+    validate(responseIdParamSchema, "params"),
+    getResponseByIdController
+)
+
+router.patch(
+    "/:id/approve",
+    validate(responseIdParamSchema, "params"),
+    validate(approveResponseSchema),
+    approveResponseController
+)
+
+router.patch(
+    "/:id/reject",
+    validate(responseIdParamSchema, "params"),
+    rejectResponseController
+)
+
+router.patch(
+    "/:id/publish",
+    validate(responseIdParamSchema, "params"),
+    togglePublishResponseController
+)
+
+router.delete(
+    "/:id",
+    validate(responseIdParamSchema, "params"),
+    deleteResponseController
+)
+
+export default router;
