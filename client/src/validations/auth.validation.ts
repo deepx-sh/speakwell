606c4db8e87b652762a63a51dee2e9b55a3bd92c
Deep Prajapati <dvprajapati7212@gmail.com>
2026-06-13 21:34:44 +0530

    feat: add validation schemas for user registration and login using Zod

---
+++ client/src/validations/auth.validation.ts
@@ -0,0 +1,36 @@
+import z from "zod";
+
+export const registerSchema = z
+  .object({
+    name: z
+      .string()
+      .trim()
+      .min(2, "Name must be at least 2 characters")
+      .max(50, "Name cannot exceed 50 characters"),
+
+    email: z
+      .string()
+      .check(z.trim(), z.email("Invalid email address"), z.toLowerCase()),
+
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters")
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        "Password must contain uppercase,lowercase,number, and speacial character",
+      ),
+    confirmPassword: z.string().min(1, "Please confirm your password"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password do not match",
+    path: ["confirmPassword"],
+  })
+
+
+  export const loginSchema = z.object({
+    email: z
+      .string()
+      .check(z.trim(), z.email("Invalid email address"), z.toLowerCase()),
+  
+    password: z.string().min(1, "Password is required"),
+  });
