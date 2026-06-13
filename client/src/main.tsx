d7354405f4f9bc80142ae68c24f9248fe7753308
Deep Prajapati <dvprajapati7212@gmail.com>
2026-06-13 21:33:15 +0530

    feat: integrate SWR for data fetching and enhance app structure with AuthProvider and AppRouter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {SWRConfig} from "swr"
 import './index.css'
-import App from './App.tsx'
+import { AuthProvider } from './context/AuthProvider.tsx'
+import { AppRouter } from './router/index.tsx'
+import  {Toaster} from "@/components/ui/sonner.tsx"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SWRConfig
+      value={{
+        revalidateOnFocus: false,
+        shouldRetryOnError:false
+      }}
+    >
+      <AuthProvider>
+        <AppRouter/>
+        <Toaster richColors position='top-center'/>
+      </AuthProvider>
+    </SWRConfig>
   </StrictMode>,
 )
