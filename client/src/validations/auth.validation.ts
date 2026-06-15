784adf740af83131f9947e2c8893c142116d176f
Deep Prajapati <dvprajapati7212@gmail.com>
2026-06-15 21:27:51 +0530

    feat: add forgot and reset password validation schemas

--- client/src/validations/auth.validation.ts
+++ client/src/validations/auth.validation.ts
@@ -34,3 +34,26 @@ export const registerSchema = z
   
     password: z.string().min(1, "Password is required"),
   });
+
+export const forgotPasswordSchema = z.object({
+     email: z
+      .string()
+      .check(z.trim(), z.email("Invalid email address"), z.toLowerCase()),
+})
+  
+export const resetPasswordSchema = z
+  .object({
+    newPassword: z
+      .string()
+      .min(8, "Password must be at least 8 characters")
+      .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        "Password must contain uppercase, lowercase, number and speacial characters"
+    ),
+    
+    confirmPassword: z
+      .string()
+      .min(1,"Please confirm your password")
+  }).refine((data) => data.newPassword === data.confirmPassword, {
+    message: "Password do not mathch",
+    path:["confirmPassword"]
+  })
